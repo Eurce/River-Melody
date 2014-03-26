8be74c33bd7ef23ca818252f01c5d14c80e5e97b
Eurce <eurce01@gmail.com>
2014-03-26 20:36:30 +0800

    Update main.cpp

--- project_code/main.cpp
+++ project_code/main.cpp
@@ -28,7 +28,7 @@ using namespace std;
 #define randomD 40
 
 
-int pixL=pixR*pixR;
+int pixL=pixR*pixR/2;
 string lo,la,z,sc;
 char str[100];
 int width,height;
@@ -491,31 +491,18 @@ void MyPropertyListenerProc(	void *							inClientData,
                             AudioFileStreamPropertyID		inPropertyID,
                             UInt32 *						ioFlags)
 {
-	// this is called by audio file stream when it finds property values
-    
 	MyData* myData = (MyData*)inClientData;
-	OSStatus err = noErr;
-    
 	switch (inPropertyID) {
 		case kAudioFileStreamProperty_ReadyToProducePackets :
 		{
-			// the file stream parser is now ready to produce audio packets.
-			// get the stream format.
 			AudioStreamBasicDescription asbd;
 			UInt32 asbdSize = sizeof(asbd);
-			err = AudioFileStreamGetProperty(inAudioFileStream, kAudioFileStreamProperty_DataFormat, &asbdSize, &asbd);
-			if (err) { PRINTERROR("get kAudioFileStreamProperty_DataFormat"); myData->failed = true; break; }
-			
-			// create the audio queue
-			err = AudioQueueNewOutput(&asbd, MyAudioQueueOutputCallback, myData, NULL, NULL, 0, &myData->audioQueue);
-			if (err) { PRINTERROR("AudioQueueNewOutput"); myData->failed = true; break; }
-			
-			// allocate audio queue buffers
+			AudioFileStreamGetProperty(inAudioFileStream, kAudioFileStreamProperty_DataFormat, &asbdSize, &asbd);
+			AudioQueueNewOutput(&asbd, MyAudioQueueOutputCallback, myData, NULL, NULL, 0, &myData->audioQueue);
 			for (unsigned int i = 0; i < kNumAQBufs; ++i) {
-				err = AudioQueueAllocateBuffer(myData->audioQueue, kAQBufSize, &myData->audioQueueBuffer[i]);
-				if (err) { PRINTERROR("AudioQueueAllocateBuffer"); myData->failed = true; break; }
+				AudioQueueAllocateBuffer(myData->audioQueue, kAQBufSize,
+                                         &myData->audioQueueBuffer[i]);
 			}
-            
 			return;
 		}
 	}
@@ -527,33 +514,21 @@ void MyPacketsProc(				void *							inClientData,
                    const void *					inInputData,
                    AudioStreamPacketDescription	*inPacketDescriptions)
 {
-	// this is called by audio file stream when it finds packets of audio
 	MyData* myData = (MyData*)inClientData;
-    
-	// the following code assumes we're streaming VBR data. for CBR data, you'd need another code branch here.
-    
 	for (int i = 0; i < inNumberPackets; ++i) {
 		SInt64 packetOffset = inPacketDescriptions[i].mStartOffset;
 		SInt64 packetSize   = inPacketDescriptions[i].mDataByteSize;
-		
-		// if the space remaining in the buffer is not enough for this packet, then enqueue the buffer.
 		size_t bufSpaceRemaining = kAQBufSize - myData->bytesFilled;
 		if (bufSpaceRemaining < packetSize) {
 			MyEnqueueBuffer(myData);
 			WaitForFreeBuffer(myData);
 		}
-		
-		// copy data to the audio queue buffer
 		AudioQueueBufferRef fillBuf = myData->audioQueueBuffer[myData->fillBufferIndex];
 		memcpy((char*)fillBuf->mAudioData + myData->bytesFilled, (const char*)inInputData + packetOffset, packetSize);
-		// fill out packet description
 		myData->packetDescs[myData->packetsFilled] = inPacketDescriptions[i];
 		myData->packetDescs[myData->packetsFilled].mStartOffset = myData->bytesFilled;
-		// keep track of bytes filled and packets filled
 		myData->bytesFilled += packetSize;
 		myData->packetsFilled += 1;
-		
-		// if that was the last free packet description, then enqueue the buffer.
 		size_t packetsDescsRemaining = kAQMaxPacketDescs - myData->packetsFilled;
 		if (packetsDescsRemaining == 0) {
 			MyEnqueueBuffer(myData);
@@ -565,9 +540,8 @@ void MyPacketsProc(				void *							inClientData,
 OSStatus StartQueueIfNeeded(MyData* myData)
 {
 	OSStatus err = noErr;
-	if (!myData->started) {		// start the queue if it has not been started already
-		err = AudioQueueStart(myData->audioQueue, NULL);
-		if (err) { PRINTERROR("AudioQueueStart"); myData->failed = true; return err; }
+	if (!myData->started) {
+		AudioQueueStart(myData->audioQueue, NULL);
 		myData->started = true;
 	}
 	return err;
@@ -576,14 +550,11 @@ OSStatus StartQueueIfNeeded(MyData* myData)
 OSStatus MyEnqueueBuffer(MyData* myData)
 {
 	OSStatus err = noErr;
-	myData->inuse[myData->fillBufferIndex] = true;		// set in use flag
-	
-	// enqueue buffer
+	myData->inuse[myData->fillBufferIndex] = true;
 	AudioQueueBufferRef fillBuf = myData->audioQueueBuffer[myData->fillBufferIndex];
 	fillBuf->mAudioDataByteSize = myData->bytesFilled;
-	err = AudioQueueEnqueueBuffer(myData->audioQueue, fillBuf, myData->packetsFilled, myData->packetDescs);
-	if (err) { PRINTERROR("AudioQueueEnqueueBuffer"); myData->failed = true; return err; }
-	
+	AudioQueueEnqueueBuffer(myData->audioQueue, fillBuf, myData->packetsFilled,
+                            myData->packetDescs);
 	StartQueueIfNeeded(myData);
 	
 	return err;
@@ -592,20 +563,6 @@ OSStatus MyEnqueueBuffer(MyData* myData)
 
 void WaitForFreeBuffer(MyData* myData)
 {
-	// go to next buffer
-	if (++myData->fillBufferIndex >= kNumAQBufs) myData->fillBufferIndex = 0;
-	myData->bytesFilled = 0;		// reset bytes filled
-	myData->packetsFilled = 0;		// reset packets filled
-    
-	// wait until next buffer is not in use
-	printf("->lock\n");
-	pthread_mutex_lock(&myData->mutex);
-	while (myData->inuse[myData->fillBufferIndex]) {
-		printf("... WAITING ...\n");
-		pthread_cond_wait(&myData->cond, &myData->mutex);
-	}
-	pthread_mutex_unlock(&myData->mutex);
-	printf("<-unlock\n");
 }
 
 int MyFindQueueBuffer(MyData* myData, AudioQueueBufferRef inBuffer)
@@ -622,13 +579,8 @@ void MyAudioQueueOutputCallback(	void*					inClientData,
                                 AudioQueueRef			inAQ,
                                 AudioQueueBufferRef		inBuffer)
 {
-	// this is called by the audio queue when it has finished decoding our data.
-	// The buffer is now free to be reused.
 	MyData* myData = (MyData*)inClientData;
-    
 	unsigned int bufIndex = MyFindQueueBuffer(myData, inBuffer);
-	
-	// signal waiting thread that the buffer is free.
 	pthread_mutex_lock(&myData->mutex);
 	myData->inuse[bufIndex] = false;
 	pthread_cond_signal(&myData->cond);
@@ -640,11 +592,9 @@ void MyAudioQueueIsRunningCallback(		void*					inClientData,
                                    AudioQueuePropertyID	inID)
 {
 	MyData* myData = (MyData*)inClientData;
-	
 	UInt32 running;
 	UInt32 size;
-	OSStatus err = AudioQueueGetProperty(inAQ, kAudioQueueProperty_IsRunning, &running, &size);
-	if (err) { PRINTERROR("get kAudioQueueProperty_IsRunning"); return; }
+	AudioQueueGetProperty(inAQ, kAudioQueueProperty_IsRunning, &running, &size);
 	if (!running) {
 		pthread_mutex_lock(&myData->mutex);
 		pthread_cond_signal(&myData->done);
